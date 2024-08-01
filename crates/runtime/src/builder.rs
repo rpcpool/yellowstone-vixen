9c524b967252a65dfc276bbd35f9d56361c84ad6
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-08-01 15:26:51 +0530

    fix: readme

--- crates/runtime/src/builder.rs
+++ crates/runtime/src/builder.rs
@@ -50,7 +50,9 @@ impl<A, X, M> RuntimeBuilder<A, X, M> {
         self
     }
 
-    pub fn opts(self, opts: IndexerOpts) -> Self { self.mutate(|s| s.opts = Some(opts)) }
+    pub fn opts(self, opts: IndexerOpts) -> Self {
+        self.mutate(|s| s.opts = Some(opts))
+    }
 
     pub fn metrics<N>(self, metrics: N) -> RuntimeBuilder<A, X, N> {
         let Self {
@@ -91,5 +93,7 @@ impl<A, X, M: MetricsBackend> RuntimeBuilder<A, X, M> {
     }
 
     #[inline]
-    pub fn build(self) -> Runtime<A, X, M> { self.try_build().unwrap() }
+    pub fn build(self) -> Runtime<A, X, M> {
+        self.try_build().unwrap()
+    }
 }
