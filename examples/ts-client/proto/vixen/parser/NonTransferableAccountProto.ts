f32f7a071a6214bb39585a48d512e38e55c54866
Fernando <fernandodeluret@gmail.com>
2025-02-10 23:48:00 -0300

    `feat` Nodejs client:   - remove static example   - add Makefile for generated code in dynamic example

--- examples/ts-client/proto/vixen/parser/NonTransferableAccountProto.ts
+++
@@ -1,8 +0,0 @@
-// Original file: /home/fernando/Documents/abklabs/yellowstone-vixen/crates/proto/proto/solana-token/accounts.proto
-
-
-export interface NonTransferableAccountProto {
-}
-
-export interface NonTransferableAccountProto__Output {
-}
