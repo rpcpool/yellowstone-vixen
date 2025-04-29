ba4e552ced6f4c522c7f5ffc5e618aa3fe59630d
Fernando <fernandodeluret@gmail.com>
2025-04-29 20:16:50 -0300

    `chore` fix lints

--- crates/raydium-amm-v4-parser/src/generated_parser/accounts_parser.rs
+++ crates/raydium-amm-v4-parser/src/generated_parser/accounts_parser.rs
@@ -69,9 +69,7 @@ impl yellowstone_vixen_core::Parser for AccountParser {
     type Input = yellowstone_vixen_core::AccountUpdate;
     type Output = RaydiumAmmProgramState;
 
-    fn id(&self) -> std::borrow::Cow<str> {
-        "raydium_amm::AccountParser".into()
-    }
+    fn id(&self) -> std::borrow::Cow<str> { "raydium_amm::AccountParser".into() }
 
     fn prefilter(&self) -> yellowstone_vixen_core::Prefilter {
         yellowstone_vixen_core::Prefilter::builder()
@@ -94,9 +92,7 @@ impl yellowstone_vixen_core::Parser for AccountParser {
 
 impl yellowstone_vixen_core::ProgramParser for AccountParser {
     #[inline]
-    fn program_id(&self) -> yellowstone_vixen_core::Pubkey {
-        ID.to_bytes().into()
-    }
+    fn program_id(&self) -> yellowstone_vixen_core::Pubkey { ID.to_bytes().into() }
 }
 
 // #[cfg(feature = "proto")]
@@ -223,8 +219,6 @@ mod proto_parser {
     impl ParseProto for AccountParser {
         type Message = proto_def::ProgramState;
 
-        fn output_into_message(value: Self::Output) -> Self::Message {
-            value.into_proto()
-        }
+        fn output_into_message(value: Self::Output) -> Self::Message { value.into_proto() }
     }
 }
