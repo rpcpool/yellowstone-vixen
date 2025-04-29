8b1749dd65abcbd1dd32011c4ff385cedbc60fcb
Fernando <fernandodeluret@gmail.com>
2025-04-29 20:07:59 -0300

    `feat` Add new set of parsers:   - meteora-amm   - moonshot   - orca-whirlpool   - pump-swaps   - raydium-amm-v4   - raydium-clmm   - raydium-cpmm

--- crates/jupiter-swap-parser/src/generated_parser/accounts_parser.rs
+++ crates/jupiter-swap-parser/src/generated_parser/accounts_parser.rs
@@ -35,9 +35,7 @@ impl yellowstone_vixen_core::Parser for AccountParser {
     type Input = yellowstone_vixen_core::AccountUpdate;
     type Output = JupiterProgramState;
 
-    fn id(&self) -> std::borrow::Cow<str> {
-        "jupiter::AccountParser".into()
-    }
+    fn id(&self) -> std::borrow::Cow<str> { "jupiter::AccountParser".into() }
 
     fn prefilter(&self) -> yellowstone_vixen_core::Prefilter {
         yellowstone_vixen_core::Prefilter::builder()
@@ -61,9 +59,7 @@ impl yellowstone_vixen_core::Parser for AccountParser {
 
 impl yellowstone_vixen_core::ProgramParser for AccountParser {
     #[inline]
-    fn program_id(&self) -> yellowstone_vixen_core::Pubkey {
-        ID.to_bytes().into()
-    }
+    fn program_id(&self) -> yellowstone_vixen_core::Pubkey { ID.to_bytes().into() }
 }
 
 // #[cfg(feature = "proto")]
@@ -98,8 +94,6 @@ mod proto_parser {
     impl ParseProto for AccountParser {
         type Message = proto_def::ProgramState;
 
-        fn output_into_message(value: Self::Output) -> Self::Message {
-            value.into_proto()
-        }
+        fn output_into_message(value: Self::Output) -> Self::Message { value.into_proto() }
     }
 }
