ad9a96105d82584a836c3145dccfdb17c70198e5
aoikurokawa <ht.so.ik0102@gmail.com>
2025-06-28 11:11:19 +0900

    fix: format

--- crates/stake-pool-parser/src/account_parser.rs
+++ crates/stake-pool-parser/src/account_parser.rs
@@ -1,12 +1,11 @@
 use spl_pod::solana_program::{self, borsh1::try_from_slice_unchecked};
-use spl_stake_pool::state::{StakePool, ValidatorList, ValidatorStakeInfo};
+use spl_stake_pool::state::{StakePool, ValidatorList};
 
 /// SplStakePool Program State
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug)]
 pub enum SplStakePoolProgramState {
     StakePool(StakePool),
-    ValidatorStakeInfo(ValidatorStakeInfo),
     ValidatorList(ValidatorList),
 }
 
@@ -16,14 +15,14 @@ impl SplStakePoolProgramState {
 
         match first_byte {
             0 => Err(yellowstone_vixen_core::ParseError::from(
-                "Invalid Account".to_owned(),
+                "Uninitialized Account".to_owned(),
             )),
             1 => {
-                let stake_pool = try_from_slice_unchecked(data_bytes)?;
+                let stake_pool = try_from_slice_unchecked::<StakePool>(data_bytes)?;
                 return Ok(SplStakePoolProgramState::StakePool(stake_pool));
             },
             2 => {
-                let validator_list = try_from_slice_unchecked(data_bytes)?;
+                let validator_list = try_from_slice_unchecked::<ValidatorList>(data_bytes)?;
                 return Ok(SplStakePoolProgramState::ValidatorList(validator_list));
             },
             _ => Err(yellowstone_vixen_core::ParseError::from(
@@ -40,9 +39,7 @@ impl yellowstone_vixen_core::Parser for AccountParser {
     type Input = yellowstone_vixen_core::AccountUpdate;
     type Output = SplStakePoolProgramState;
 
-    fn id(&self) -> std::borrow::Cow<str> {
-        "spl_stake_pool::AccountParser".into()
-    }
+    fn id(&self) -> std::borrow::Cow<str> { "spl_stake_pool::AccountParser".into() }
 
     fn prefilter(&self) -> yellowstone_vixen_core::Prefilter {
         yellowstone_vixen_core::Prefilter::builder()
@@ -65,7 +62,5 @@ impl yellowstone_vixen_core::Parser for AccountParser {
 
 impl yellowstone_vixen_core::ProgramParser for AccountParser {
     #[inline]
-    fn program_id(&self) -> yellowstone_vixen_core::Pubkey {
-        spl_stake_pool::ID.to_bytes().into()
-    }
+    fn program_id(&self) -> yellowstone_vixen_core::Pubkey { spl_stake_pool::ID.to_bytes().into() }
 }
