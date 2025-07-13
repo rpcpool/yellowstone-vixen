2acad072d55280b9845d6966e9275903736336ea
Fernando <fernandodeluret@gmail.com>
2025-07-13 17:29:09 -0300

    `chore` fix fmt lint

--- crates/raydium-amm-v4-parser/src/generated_parser/instructions_parser.rs
+++ crates/raydium-amm-v4-parser/src/generated_parser/instructions_parser.rs
@@ -71,9 +71,7 @@ impl yellowstone_vixen_core::Parser for InstructionParser {
     #[cfg(feature = "shared-data")]
     type Output = InstructionUpdateOutput<RaydiumAmmV4ProgramIx>;
 
-    fn id(&self) -> std::borrow::Cow<str> {
-        "RaydiumAmmV4::InstructionParser".into()
-    }
+    fn id(&self) -> std::borrow::Cow<str> { "RaydiumAmmV4::InstructionParser".into() }
 
     fn prefilter(&self) -> yellowstone_vixen_core::Prefilter {
         yellowstone_vixen_core::Prefilter::builder()
@@ -112,9 +110,7 @@ impl yellowstone_vixen_core::Parser for InstructionParser {
 
 impl yellowstone_vixen_core::ProgramParser for InstructionParser {
     #[inline]
-    fn program_id(&self) -> yellowstone_vixen_core::Pubkey {
-        ID.to_bytes().into()
-    }
+    fn program_id(&self) -> yellowstone_vixen_core::Pubkey { ID.to_bytes().into() }
 }
 
 impl InstructionParser {
