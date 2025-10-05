1f956d37f3bb58b5d1476533e73c8ee8d277e1dc
Harry Chen <a00012025@gmail.com>
2025-10-05 21:29:58 +0800

    feat: jup limit order v1

---
+++ crates/jupiter-limit-order-v1/src/lib.rs
@@ -0,0 +1,17 @@
+mod generated_parser;
+mod generated_sdk;
+pub use generated::*;
+pub use generated_parser::*;
+use generated_sdk as generated;
+use solana_pubkey::Pubkey;
+
+pub const ID: Pubkey = LIMIT_ORDER_ID;
+
+// #[cfg(feature = "proto")]
+pub mod proto_def {
+    #![allow(clippy::large_enum_variant)]
+
+    tonic::include_proto!("vixen.parser.jupiter_limit_order_v1");
+
+    pub const DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("descriptor");
+}
