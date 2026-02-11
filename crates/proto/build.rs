c8d55088336aa955f74893d2e1788ca316466d6c
Kino <kino@raccoons.dev>
2026-02-11 19:04:43 +0800

    - update prost/tonic versions to 0.14.0 (#193)
    - use tonic prost build instead of tonic build
    - remove prost-build and tonic-build
    - remove the install Protoc from CI because protobuf-src already bundles protoc
    - fix parser feature

--- crates/proto/build.rs
+++ crates/proto/build.rs
@@ -3,13 +3,27 @@ use std::{env, path::PathBuf};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
+    #[cfg(any(feature = "parser", feature = "stream"))]
+    unsafe {
+        std::env::set_var("PROTOC", protobuf_src::protoc());
+    }
+
+    #[cfg(feature = "parser")]
+    {
+        tonic_prost_build::configure()
+            .file_descriptor_set_path(out_dir.join("vixen.parser.token.bin"))
+            .compile_protos(&["proto/token.proto"], &["proto"])
+            .unwrap();
+
+        tonic_prost_build::configure()
+            .file_descriptor_set_path(out_dir.join("vixen.parser.token_extensions.bin"))
+            .compile_protos(&["proto/token_extensions.proto"], &["proto"])
+            .unwrap();
+    }
+
     #[cfg(feature = "stream")]
     {
-        unsafe {
-            std::env::set_var("PROTOC", protobuf_src::protoc());
-        }
-
-        tonic_build::configure()
+        tonic_prost_build::configure()
             .file_descriptor_set_path(out_dir.join("stream_descriptor.bin"))
             .compile_protos(&["proto/stream.proto"], &["proto"])
             .unwrap();
