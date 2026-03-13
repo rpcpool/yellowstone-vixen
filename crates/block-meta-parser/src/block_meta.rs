55dc7e978aa924f182d59538d6a000a258fc5582
Orex <orex@raccoons.dev>
2026-03-13 16:13:55 +0100

    nit

--- crates/block-meta-parser/src/block_meta.rs
+++ crates/block-meta-parser/src/block_meta.rs
@@ -77,12 +77,10 @@ impl Parser for BlockMetaParser {
 }
 
 impl ProgramParser for BlockMetaParser {
-    /// "B111111111111111111111111111111111111111112"
     #[inline]
     fn program_id(&self) -> Pubkey {
-        Pubkey::new([
-            2, 143, 206, 223, 9, 17, 53, 163, 33, 32, 251, 255, 120, 243, 177, 49, 160, 203, 100,
-            118, 223, 255, 122, 65, 91, 88, 104, 0, 0, 0, 0, 1,
-        ])
+        "B111111111111111111111111111111111111111112"
+            .parse()
+            .unwrap()
     }
 }
