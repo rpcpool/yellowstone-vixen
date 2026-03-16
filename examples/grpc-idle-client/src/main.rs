b1b35a6a76bed4f7c6e89ff9ec4aa526fbe0d92a
kino <kino@raccoons.dev>
2026-03-16 17:42:21 +0100

    add config to client

--- examples/grpc-idle-client/src/main.rs
+++ examples/grpc-idle-client/src/main.rs
@@ -309,10 +309,12 @@ async fn connect_client(
         .accept_compressed(config.accept_compression.unwrap_or_default().into())
         .connect_timeout(timeout)
         .timeout(timeout)
-        .http2_keep_alive_interval(Duration::from_secs(10))    // send HTTP/2 PING every 10s
-        .keep_alive_timeout(Duration::from_secs(20))           // fail if no PING ACK within 20s
-        .keep_alive_while_idle(true)                   // ping even when no RPCs in-flight
-        .tcp_keepalive(Some(Duration::from_secs(15)))          // OS-level TCP keepalive too
+        .initial_stream_window_size(16 * 1024 * 1024)        // 16 MiB
+        .initial_connection_window_size(32 * 1024 * 1024)    // 32 MiB
+        .http2_keep_alive_interval(Duration::from_secs(10))  // send HTTP/2 PING every 10s
+        .keep_alive_timeout(Duration::from_secs(20))         // fail if no PING ACK within 20s
+        .keep_alive_while_idle(true)                         // ping even when no RPCs in-flight
+        .tcp_keepalive(Some(Duration::from_secs(15)))        // OS-level TCP keepalive too
         .tls_config(ClientTlsConfig::new().with_native_roots())?
         .connect()
         .await?;
