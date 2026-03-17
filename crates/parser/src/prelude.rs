405baa1da972e1313eba1743505b4705d5936860
Orex <orex@raccoons.dev>
2026-03-17 16:51:00 +0100

    Remove useless dep

--- crates/parser/src/prelude.rs
+++ crates/parser/src/prelude.rs
@@ -1,6 +1,5 @@
 pub use std::collections::{HashMap, HashSet};
 
-#[cfg(feature = "anchor-events")]
 pub use base64;
 pub use yellowstone_vixen_core::{
     instruction, AccountUpdate, KeyBytes, ParseError, ParseResult, Parser, Prefilter, Pubkey,
